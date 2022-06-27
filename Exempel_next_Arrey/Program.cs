ea346a8aef44512baa426f66b5e0856bb76684f9
sergei sam <ssi1970@mail.ru>
2022-06-27 16:30:43 +0700

    Exempel next Arrey

---
+++ Exempel_next_Arrey/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = {11, 12, 13, 41, 15, 56, 17, 98};
+
+int n = array.Length;
+int find = 17;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+      Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
